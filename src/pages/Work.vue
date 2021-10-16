67c3ecbc16615242e4940195c59d06b6faacfd9d
David Leininger <davidleininger@hey.com>
2021-10-15 23:03:29 -0500

    Work Grid

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -2,12 +2,12 @@
   <Layout class="landing list">
     <header class="flex">
       <h1 class="text-black dark:text-white text-5xl font-mono mb-2 md:pr-4">Work</h1>
-      <button>All</button>
+      <!-- <button>All</button>
       <button>Branding</button>
       <button>Design</button>
-      <button>Development</button>
+      <button>Development</button> -->
     </header>
-    <ul class="grid grid-cols-3 gap-4">
+    <ul class="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-4">
       <li v-for="item in $page.work.edges" :key="item.node.id">
         <g-link :to="item.node.path" class="group flex flex-col justify-between font-mono text-black hover:text-purple-dark focus:text-purple dark:text-white dark:hover:text-yellow dark:focus:text-yellow text-xl">
         <g-image :src="item.node.img.src" :alt="`${$page.work.title} preview image`" class="mb-2" />{{ item.node.title }}</g-link>
