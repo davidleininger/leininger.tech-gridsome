c336de0db80f8d8432af1833d81e72cf437c631b
David Leininger <davidleininger@hey.com>
2021-10-16 01:58:59 -0500

    split up homepage - new ideas to come

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,7 @@
       <p class="text-xxs sm:ext-xs my-0 dark:text-grey-dark">This is some small text.</p>
     </div>
   </section>
-  <section class="flex flex-col sm:flex-row gap-6">
+  <section class="flex flex-col sm:flex-row gap-6 border-t border-teal pt-8">
     <div class="flex-1">
       <h3 class="text-2xl text-teal-dark dark:text-teal font-mono">Somewhat Recent Writings</h3>
       <p>TODO: List 3 things and then see all link</p>
