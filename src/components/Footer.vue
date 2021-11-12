2d70023ce6bb6b284f0c28a5e8b9dbe9bb6d6119
David Leininger <davidleininger@hey.com>
2021-11-12 13:33:57 -0600

    Aria labels for nav

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="bg-grey-light dark:bg-black-light flex flex-col items-center p-4 text-grey-darkest dark:text-grey-dark sm:flex-row sm:justify-between">
-    <nav class="flex">
+    <nav class="flex" aria-label="Social Links">
       <a v-for="link in socialLinks" :key="link.network" class="h-6 w-6 mr-4 last:mr-0 hover:text-teal transform hover:-translate-y-1 transition-transform duration-200" :aria-label="link.network" :href="link.url">
         <component :is="link.network"></component>
       </a>
     </nav>
     <div class="flex flex-col xs:flex-row items-center mt-4 sm:mt-0 w-full xs:w-auto">
-      <nav class="flex xs:mr-4 mb-3 xs:mb-0 justify-center xs:justify-start border-t border-b border-gray dark:border-black xs:border-b-0 xs:border-t-0 w-full xs:w-auto">
+      <nav class="flex xs:mr-4 mb-3 xs:mb-0 justify-center xs:justify-start border-t border-b border-gray dark:border-black xs:border-b-0 xs:border-t-0 w-full xs:w-auto" aria-label="Secondary Navigation">
         <g-link v-for="(link, index) in nav" :key="link.index" :to="link.url" class="px-2 h-10 flex items-center relative hover:text-teal-dark dark:hover:text-teal">{{ link.text }}</g-link>
       </nav>
       <p class="m-0">&copy; David Leininger {{ year }}</p>
