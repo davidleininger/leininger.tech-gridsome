52768b9edca2bf7ca586decf1b0d5080ee53eb0a
David Leininger <davidleininger@hey.com>
2021-11-18 12:12:31 -0600

    Aria-pressed should always be there

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,7 @@
         </li>
       </ul>
     </nav>
-    <button @click="changeMode" class="toggle flex justify-center items-center h-10 w-10 flex-shrink-0 relative" aria-label="Color Mode Toggle" :aria-pressed="isDarkMode">
+    <button @click="changeMode" class="toggle flex justify-center items-center h-10 w-10 flex-shrink-0 relative" aria-label="Color Mode Toggle" :aria-pressed="isDarkMode.toString()">
       <Toggle v-if="isDarkMode != null" :is-dark="isDarkMode" />
     </button>
   </section>
