36ce66936e58ce1c46b105fb0b1b49d429f18e3b
David Leininger <davidleininger@hey.com>
2021-11-18 12:33:34 -0600

    actually set to true or false null breaks the build

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,7 @@
         </li>
       </ul>
     </nav>
-    <button @click="changeMode" class="toggle flex justify-center items-center h-10 w-10 flex-shrink-0 relative" aria-label="Color Mode Toggle" :aria-pressed="isDarkMode.toString()">
+    <button @click="changeMode" class="toggle flex justify-center items-center h-10 w-10 flex-shrink-0 relative" aria-label="Color Mode Toggle" :aria-pressed="isDarkMode ? 'true' : 'false'">
       <Toggle v-if="isDarkMode != null" :is-dark="isDarkMode" />
     </button>
   </section>
