bc23ddd8a9b48e1ab22e6b36cf2c66d1571db662
David Leininger <davidleininger@hey.com>
2021-11-20 14:49:44 -0600

    404 page.

---
+++ src/pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout class="list">
+    <h1 class="text-center font-bold">404</h1>
+    <p class="text-center">Looks like one of us made a mistake. I'll take the credit here. Sorry about that.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: calc(10vw + 6rem);
+  color: var(--accent);
+}
+</style>
