9cddbf19e2b5fa7604f3e0b092899d155411ac48
David Leininger <davidleininger@hey.com>
2022-05-23 07:16:45 -0500

    Update to new role

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,14 +9,14 @@
         </p>
 
         <p>
-          I am a <span class="strike">Senior Developer</span> Manager at <a href="https://wwt.com">World Wide Technology</a>, where I help design and build websites and apps. When I'm not at work, I volunteer with the live streaming at church, and I help run production at several Catholic youth conferences around the country. I live in St. Louis with my amazing wife and <span class="strike">daughter</span>&nbsp;<span class="strike">two</span> three daughters. They are all too good for me, but slum it with me because I’m funny, I have power tools, and I'm an average cook. #girdad
+          I am a <s>Senior Developer</s>&nbsp;<s>Manager</s> UX Architect at <a href="https://wwt.com">World Wide Technology</a>, where I help design and build websites and apps. When I'm not at work, I volunteer with the live streaming at church, and I help run production at several Catholic youth conferences around the country. I live in St. Louis with my amazing wife and <s>daughter</s>&nbsp;<s>two</s> three daughters. They are all too good for me, but slum it with me because I’m kind of funny, I have power tools, and I'm an average cook. #girdad
         </p>
 
         <h2 class="text-2xl mt-8 text-teal-dark dark:text-teal font-mono">
           Extra Credit:
         </h2>
         <p>
-          I am a diehard Blues fan, LFC fan, and recently became a St Louis City SC fan.  I'm a self-proclaimed pizza sommelier, and I could spend hours talking about the merits of pizza. I believe that breakfast food is the best kind of food. I don’t drink coffee, but I love coffee shops. I eat cupcakes by turning them into sandwiches. I like good bourbon. I have a lot of tattoos. I like building things with power tools. I've been to 49 states – here's looking at you Alaska.
+          I am a diehard Blues fan, LFC fan, and recently became a St Louis City SC fan. I'm a self-proclaimed pizza sommelier, and I could spend hours talking about the merits of pizza. I believe that breakfast food is the best kind of food. I don’t drink coffee, but I love coffee shops. I eat cupcakes by turning them into sandwiches. I like good bourbon. I have a lot of tattoos. I like building things with power tools. I've been to 49 states – here's looking at you Alaska.
         </p>
       </div>
       <g-image class="w-full md:w-2/5 lg:w-1/3 mb-6" src="~/assets/images/rad-fam.jpg" alt=".My family"/>
